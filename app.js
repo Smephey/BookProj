e811b51824f30a8d390c899503388ecb0aae0a76
Michal Hajdo <smephey@gmail.com>
2020-04-03 16:35:59 +0100

    Added bootstrap and lint

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ var express = require('express');
 var chalk = require('chalk');
 var morgan = require('morgan');
 var path = require('path');
+var debug = require('debug')('app');
 
 var app = express();
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist')));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 })
 
 app.listen(3000, function(){
-    console.log(`listening on port ${chalk.green('3000')}`);
+    debug(`listening on port ${chalk.green('3000')}`);
 });
