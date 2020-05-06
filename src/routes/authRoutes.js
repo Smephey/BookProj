b0b479cebe0a0fd29799142ae2cd0369edd4bbfe
Michal Hajdo <smephey@gmail.com>
2020-05-06 12:34:05 +0100

    Adding a form for signing up

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const authRouter = express.Router();
+const debug = require('debug')('app:authRoutes');
+
+function router() {
+  authRouter.route('/signUp')
+    .post((req, res) => {
+      debug(req.body);
+      res.json(req.body);
+    });
+  return authRouter;
+}
+
+module.exports = router;
